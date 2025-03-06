66ba2027a5e3ea87e95f153badd6c1a40c1191a8
root <root@DESKTOP-V4EO9N9.>
2025-03-06 17:57:33 +0000

    added file to folder

---
+++ src/ft_atoll.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoll.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmiguelo <mmiguelo@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 17:53:01 by mmiguelo          #+#    #+#             */
+/*   Updated: 2025/03/06 17:53:01 by mmiguelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/**
+ * @brief Converts a string to a long long: ASCII to long long
+ * 
+ * @param str A pointer to the string to be converted
+ * @return Returns the converted long long integer value
+ */
+long	ft_atol(const char *str)
+{
+	int			i;
+	long long	sign;
+	long long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	if ((str[i] == '+') || (str[i] == '-'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	if ((str[i] == '+') || (str[i] == '-'))
+		return (0);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
+}
